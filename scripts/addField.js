eb35a034d7fffaa2d1fbfe312da278027605904b
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2020-08-06 16:47:02 -0300

    Aula 3 (#2)
    * aula-3
    
    * fim-html-css
    
    * Aula-3

---
+++ scripts/addField.js
@@ -0,0 +1,20 @@
+// procurar o botao
+document.querySelector("#add-time").addEventListener('click', cloneField);
+// executar uma acao
+function cloneField() {
+    console.log('aqui')
+    // Node serve para pegar elementos HTML
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);
+
+    // pegar os campos
+    const fields = newFieldContainer.querySelectorAll('input');
+    // para cada
+    fields.forEach(function (field) {
+        field.value = "";
+    });
+
+    // colocar na pagina
+    document.querySelector('#schedule-items').appendChild(newFieldContainer);
+
+
+}
