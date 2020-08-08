8f83307114108ae762ec7c78e75b8e927072a1c4
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2020-08-08 18:26:03 -0300

    Aula 5 (#4)
    * Aula-5 pausa proalmoço
    
    * Hora de dormir
    
    * Fim aula 5
    
    * Update database.sqlite

---
+++ src/database/test.js
@@ -0,0 +1,60 @@
+const Database = require('./db.js');
+const creatProffy = require('./creatProffy');
+
+Database.then(async (db) => {
+    // inserir dados
+
+    proffyValue = {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "91984719579",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    }
+
+    classValue = {
+        subject: 1,
+        cost: "20",
+        // Proffy id virá pelo banco de dados
+    }
+
+    classScheduleValues = [
+        // class id vem pelo banco de dados
+        {
+            weekday: 1,
+            time_from: 720,
+            time_to: 1220
+        },
+        {
+            weekday: 0,
+            time_from: 520,
+            time_to: 1220
+        }
+    ]
+    // await creatProffy(db, { proffyValue, classValue, classScheduleValues })
+
+    // consultar os dados inseridos
+    // todos os porffys
+    const selectedProffys = await db.all("SELECT * FROM proffys");
+    // console.log(selectedProffys)
+
+    // Consultar as classes de um determinado professor e trazer junto os dados dele
+    const selectClassesAndProffys = await db.all(`
+    SELECT classes.*, proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE classes.proffy_id = 1;
+    `);
+    // console.log(selectClassesAndProffys)
+
+    // time_from <= horario_solicitado < time_to
+    const selectClassesSchedules = await db.all(`
+    SELECT class_schedule.*
+    FROM class_schedule
+    WHERE class_schedule.class_id = 1
+    AND class_schedule.weekday = "0"
+    AND class_schedule.time_from <= "420"
+    AND class_schedule.time_to > "520"
+    `)
+
+    // console.log(selectClassesSchedules)
+})
--- src/server.js
+++ src/server.js
@@ -1,105 +1,26 @@
-// dados
-const proffys = [
-    {
-        name: "Diego Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "91984719579",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Quimica",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220],
-    },
-    {
-        name: "Daniele Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "91984719579",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Quimica",
-        cost: "20",
-        weekday: [1],
-        time_from: [720],
-        time_to: [1220],
-    },
-    {
-        name: "Mayke Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "91984719579",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Quimica",
-        cost: "20",
-        weekday: [1],
-        time_from: [720],
-        time_to: [1220],
-    }
-]
-const subjects = [
-    "Biologia",
-    "Artes",
-    "Ciências",
-    "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
 
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-// funcionalidades
-function getSubjects(subjectNumber) {
-    const arrayPosition = subjectNumber - 1;
-    return subjects[arrayPosition]
-}
-function pageLanding(req, res) {
-    // req.query vem as informações da pagina, no caso o form
-    //  __dirname retorna caminho do diretorio que esta o arquivo local
-    return res.render("index.html");
-}
-function pageStudy(req, res) {
-    const filters = req.query;
-    return res.render("study.html", { proffys, filters, subjects, weekdays });
-}
-function pageGiveClass(req, res) {
-    const data = req.query;
-    const isNotEmpty = Object.keys(data).length > 0;
-    // adiconar dados a lista de proffys
-    if (isNotEmpty) {
-        data.subject = getSubjects(data.subject)
-        proffys.push(data);
-
-        return res.redirect("/study");
-    } else {
-        return res.render("give-classes.html", { subjects, weekdays });
-    }
-}
-
+// servidor
 const express = require('express')
 const server = express()
-const nunjucks = require('nunjucks')
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
+
 // configurar nujucks
+const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
     express: server,
     noCache: true,
 })
 
-// servidor
+
 server
+    // receber os dados do req.body, pois padrão é req.query
+    .use(express.urlencoded({ extended: true }))
     // configurar  arquivos estaticos (css, scripts, imagens)
     .use(express.static("public"))
     // (req- é o que preciso, res- é o que exibe na tela) rotas das aplicações
     .get("/", pageLanding)
     .get("/study", pageStudy)
-    .get("/give-classes", pageGiveClass)
+    .get("/give-classes", pageGiveClasses)
+    .post("/save-classes", saveClasses)
     .listen(5500)
