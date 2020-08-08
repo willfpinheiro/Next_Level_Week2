ee6ca96d50965e9392449e091e0f6f9d2469a850
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2020-08-08 18:07:49 -0300

    Fim aula 5

--- src/database/creatProffy.js
+++ src/database/creatProffy.js
@@ -1,7 +1,5 @@
-// para funcionar o await precisamos usar o async
 module.exports = async function (db, { proffyValue, classValue, classScheduleValues }) {
-    // inserir dados na tabela de teachers
-    // espera a funçao fazer a sua função.
+    //inserir dados na tableda de proffys
     const insertedProffy = await db.run(`
         INSERT INTO proffys (
             name,
@@ -19,39 +17,39 @@ module.exports = async function (db, { proffyValue, classValue, classScheduleVal
     const proffy_id = insertedProffy.lastID
 
     // inserir dados na tabela classes
+
     const insertedClass = await db.run(`
-    INSERT INTO classes (
-        subject,
-        cost,
-        proffy_id
-    ) VALUES (
-        "${classValue.subject}",
-        "${classValue.cost}",
-        "${proffy_id}"
-    );
-`)
+            INSERT INTO classes (
+                subject,
+                cost,
+                proffy_id
+            ) VALUES (
+                "${classValue.subject}",
+                "${classValue.cost}",
+                "${proffy_id}"
+            );
+    `)
 
     const class_id = insertedClass.lastID
 
-    // inserir dados na tabela classes
+    // Inserir dados na tabela class_schedule
     const insertedAllClassScheduleValues = classScheduleValues.map((classScheduleValue) => {
         return db.run(`
-        INSERT INTO classes_schedule (
-            class_id,
-            weekday,
-            time_from,
-            time_to
-        ) VALUES (
-            "${class_id}",
-            "${classScheduleValue.weekday}",
-            "${classScheduleValue.time_from}",
-            "${classScheduleValue.time_to}"
-        );
+            INSERT INTO class_schedule (
+                class_id,
+                weekday,
+                time_from,
+                time_to
+            ) VALUES (
+                "${class_id}",
+                "${classScheduleValue.weekday}",
+                "${classScheduleValue.time_from}",
+                "${classScheduleValue.time_to}"
+            );
         `)
     })
 
-    // aqui vou executar todos os db.runs() das class_schedule
-    // promisse.all serve para esperar todas as execuções
+    //aqui vou executar todos os db.runs() das class_schedules
     await Promise.all(insertedAllClassScheduleValues)
 
-};
+}
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-const { pageLanding, pageStudy, pageGiveClass } = require('./pages')
 
 // servidor
 const express = require('express')
 const server = express()
 
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
 // configurar nujucks
 const nunjucks = require('nunjucks')
@@ -14,10 +14,13 @@ nunjucks.configure('src/views', {
 
 
 server
+    // receber os dados do req.body, pois padrão é req.query
+    .use(express.urlencoded({ extended: true }))
     // configurar  arquivos estaticos (css, scripts, imagens)
     .use(express.static("public"))
     // (req- é o que preciso, res- é o que exibe na tela) rotas das aplicações
     .get("/", pageLanding)
     .get("/study", pageStudy)
-    .get("/give-classes", pageGiveClass)
+    .get("/give-classes", pageGiveClasses)
+    .post("/save-classes", saveClasses)
     .listen(5500)
